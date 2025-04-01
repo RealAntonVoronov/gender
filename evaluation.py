be0f7765e6f2b1fc64c4167451d946d95348577a
realantonvoronov <avoronov3797@gmail.com>
2025-04-01 19:12:17 +0300

    return normalization in rouge_score calculation

--- evaluation.py
+++ evaluation.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import json
 import nltk
 import evaluate
@@ -8,15 +9,15 @@ nltk.download('punkt_tab')
 rouge_score = evaluate.load("rouge")
 
 def compute_rouge(preds, labels):
-    # preds = [sent_tokenize(pred) for pred in preds]
-    # labels = [sent_tokenize(label) for label in labels]
-    # decoded_preds = ["\n".join(pred) for pred in preds]
-    # decoded_labels = ["\n".join(label) for label in labels]
+    preds = [sent_tokenize(pred) for pred in preds]
+    labels = [sent_tokenize(label) for label in labels]
+    decoded_preds = ["\n".join(pred) for pred in preds]
+    decoded_labels = ["\n".join(label) for label in labels]
 
     # Compute ROUGE scores
     result = rouge_score.compute(
-        predictions=preds,
-        references=labels,
+        predictions=decoded_preds,
+        references=decoded_labels,
         use_stemmer=True
     )
     # Extract the median scores
@@ -26,8 +27,8 @@ def compute_rouge(preds, labels):
 
 model_dir = "results/biocarta_kegg-large-perm20-neg0.01-add_rouge/"
 if __name__ == "__main__":
-    with open(f"{model_dir}/test_preds") as f:
-        preds = f.readlines()
-    with open(f"{model_dir}/test_labels") as f:
-        labels = f.readlines()
-    print(compute_rouge(preds, labels))
+    with open(f"{model_dir}/test_preds", "r") as f:
+        last_preds = f.readlines()
+
+    labels = pd.read_csv("data/test/val_clusters.csv")["full"]
+    print(compute_rouge(last_preds, labels))
